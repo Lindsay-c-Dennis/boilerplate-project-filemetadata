9a07d556ea3bab42320b1c6b42ce0fbd164d0fc2
Lindsay Dennis <30577902+Lindsay-c-Dennis@users.noreply.github.com>
2019-05-13 12:22:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 
 var express = require('express');
 var cors = require('cors');
-
-// require and use "multer"...
-
+const bodyParser = require('body-parser');
+const multer = require("multer");
+const upload = multer({ dest: 'uploads/' });
 var app = express();
-
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -17,6 +17,10 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({filename: req.file.originalname, type: req.file.mimetype, size: req.file.size})
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
